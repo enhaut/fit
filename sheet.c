50899c2bda70f7ca8e87332e947dc675a02dc0b9
Samuel Dobroň <samulkodob@gmail.com>
2020-10-15 17:48:29 +0200

    checking columns requirements

--- sheet.c
+++ sheet.c
@@ -34,11 +34,27 @@ bool defined_delimiter(int args_count, char *arguments[])
     return is_defined_delimiter;
 }
 
+int check_column_requirements(int column_size, int column_index, int column_count, int row_index, char *remaining_row)
+{
+    int return_code = 0;
+    if (column_size > CELL_SIZE - 1)  // 1 bite is reserved for \0, -1 because of that
+    {
+        printf("Column is bigger than allowed!\n");
+        return_code = -1;
+    }else if (column_index > MAX_COLUMNS){
+        printf("You are trying to use more columns than allowed!\n");
+        return_code = -2;
+    }else if (column_index + 1 != column_count && row_index > 0 && remaining_row == NULL){
+        // +1 because column index is indexed from 0 and column_count from 1, chechking of remaining_row to make sure, actual column is last one
+        printf("You have inconsistent column count!\n");
+        return_code = -3;
+    }
+    return return_code;
+}
+
 
 int process_row(char *row, char *delimiter, int row_index, int *columns_count)
 {
-    (void)row_index;
-    (void)columns_count;
     int delimiter_size = strlen(delimiter);
     char *remaining_row = row;
 
@@ -62,6 +78,10 @@ int process_row(char *row, char *delimiter, int row_index, int *columns_count)
             column_size = strlen(original_row) - strlen(remaining_row);
             remaining_row += delimiter_size;  // move pointer behind the delimiter to force looking for delimiter, behind actual column
         }
+
+        int column_requirements = check_column_requirements(column_size, column_index, *columns_count, row_index, remaining_row);
+        if (column_requirements < 0)
+            return column_requirements;
 
         if (column_size <= 0)
         {
@@ -108,7 +128,9 @@ int main(int args_count, char *arguments[])
         if ((character == '\n' || character == '\r' || character == EOF) && row_buffer_position > 0)
         {
             row_buffer[row_buffer_position] = '\0';
-            process_row(row_buffer, cells_delimiter, row_index, &column_count);
+            int result_of_processing = process_row(row_buffer, cells_delimiter, row_index, &column_count);
+            if (result_of_processing != 0)
+                return result_of_processing;
             row_index++;
             row_buffer_position = 0;
         }else{
