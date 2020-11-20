0e9e203ecd53a0917c374887a4b110d6ee57c719
Samuel Dobroň <samulkodob@gmail.com>
2020-11-20 17:33:58 +0100

    getting delimiter

--- sps.c
+++ sps.c
@@ -5,10 +5,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 #define print_error(...) fprintf(stderr, __VA_ARGS__)
 #define MINIMAL_ARGUMENTS_COUNT 3
 
+bool string_compare(char *first, char *second)
+{
+    return strcmp(first, second) == 0;
+}
 
 bool provided_minimal_amount_of_arguments(int arg_count)
 {
@@ -18,10 +23,40 @@ bool provided_minimal_amount_of_arguments(int arg_count)
     return true;
 }
 
+// Returns -1 for invalid delimiter -> exit, 0 for no delimiter defined, 1 for defined delimiter
+int defined_delimiter(int arg_count, char *arguments[])
+{
+    bool is_set_d_argument = string_compare(arguments[1], "-d");
+
+    if (arg_count == 4 || (arg_count == 5 &&
+                          (((is_set_d_argument && strlen(arguments[2]) == 0) || // delimiter value is empty
+                            !is_set_d_argument) ||                                 // 2. argument is not -d
+                            (strchr(arguments[2], '"') || strchr(arguments[2], '\\')   // forbidden chars in delimiter
+                            ))))
+    {
+        print_error("Invalid delimiter!\n");
+        return -1;
+    }else if (arg_count == 5 && is_set_d_argument)
+        return 1;
+
+    return 0;
+}
+
 int main(int arg_count, char *arguments[])
 {
     if (provided_minimal_amount_of_arguments(arg_count))
         return EXIT_FAILURE;
 
+    int is_defined_delimiter = defined_delimiter(arg_count, arguments);
+    if (is_defined_delimiter == -1)
+        return EXIT_FAILURE;
+
+    char default_delimiter[] = " ";
+    char *delimiter = default_delimiter;
+
+    if (is_defined_delimiter)
+        delimiter = arguments[2];
+
+    printf(".%s.", delimiter);
     return EXIT_SUCCESS;
 }
