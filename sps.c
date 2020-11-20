a578dcca25411a63711b5aa9f747d297cbc2ce7d
Samuel Dobroň <samulkodob@gmail.com>
2020-11-20 17:33:05 +0100

    arguments count checking

--- sps.c
+++ sps.c
@@ -1,10 +1,27 @@
 //
-// Created by Samuel Dobron on 17. 11. 2020.
+// Created by enhaut on 17. 11. 2020.
 //
 
+#include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+#define print_error(...) fprintf(stderr, __VA_ARGS__)
+#define MINIMAL_ARGUMENTS_COUNT 3
+
+
+bool provided_minimal_amount_of_arguments(int arg_count)
+{
+    if (arg_count >= MINIMAL_ARGUMENTS_COUNT)
+        return false;
+    print_error("Invalid arguments!");
+    return true;
+}
 
 int main(int arg_count, char *arguments[])
 {
+    if (provided_minimal_amount_of_arguments(arg_count))
+        return EXIT_FAILURE;
+
     return EXIT_SUCCESS;
 }
