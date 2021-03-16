9e293e0fe5747cb8c15b2e8fbc9f1303dd4cdcc6
Samuel Dobroň <samulkodob@gmail.com>
2021-03-16 08:17:22 +0100

    moved printing of the last 10. prime numbers

--- 1/src/primes.c
+++ 1/src/primes.c
@@ -13,8 +13,17 @@ int main(void)
     clock_t start = clock();
 
     bitset_alloc(bitset_array, 200000000);
-    //printf("%lu", bitset_size(bitset_array));
     Eratosthenes(bitset_array);
+
+    int got_primes = 0;
+    for (bitset_index_t i = 0; i < bitset_size(bitset_array) && got_primes < 10; i++)
+    {
+        if (bitset_getbit(bitset_array, (bitset_size(bitset_array) - i)))
+            continue;
+
+        printf("%ld\n", bitset_size(bitset_array) -i);
+        got_primes++;
+    }
 
     fprintf(stderr, "Time=%.3g\n", (double)(clock() - start) / CLOCKS_PER_SEC);
     return 0;
