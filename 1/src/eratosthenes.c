d4227c2732e23b3462f6529b368c5855cade74ad
Samuel Dobroň <samulkodob@gmail.com>
2021-03-18 22:13:24 +0100

    optimalization

--- 1/src/eratosthenes.c
+++ 1/src/eratosthenes.c
@@ -11,9 +11,6 @@
 
 void Eratosthenes(bitset_t pole)
 {
-    bitset_setbit(pole, 0, 1);  // number "0" is not even
-    bitset_setbit(pole, 1, 1);  // number "1" also
-
     bitset_index_t available_values = bitset_size(pole);  // faster than using macro everywhere (~20ms on my machine at average of 200tries)
     memset(&(pole[1]), 0x55, available_values / 8 + 1); // +1 because of not using the first element in array
 
