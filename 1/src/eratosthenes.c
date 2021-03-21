169a7826700a5f538e1f77acb3e47c89badbf62e
Samuel Dobroň <samulkodob@gmail.com>
2021-03-21 15:12:21 +0100

    optimalization

--- 1/src/eratosthenes.c
+++ 1/src/eratosthenes.c
@@ -12,15 +12,19 @@
 void Eratosthenes(bitset_t pole)
 {
     bitset_index_t available_values = bitset_size(pole);  // faster than using macro everywhere (~20ms on my machine at average of 200tries)
-    memset(&(pole[1]), 0x55, available_values / 8 + 1); // +1 because of not using the first element in array
+    size_t array_elements = (available_values / CHAR_BIT);
+    memset(&(pole[1]), 0x55, array_elements);
+
+    for (bitset_index_t i = array_elements * CHAR_BIT; i < available_values; i++)
+        bitset_setbit(pole, i, 1);  // setting numbers dividable by 2 of last array element
 
     bitset_index_t size = (bitset_index_t)sqrt(available_values);
-    for (bitset_index_t i = 2; i < size; i++)
+    for (bitset_index_t i = 3; i < size; i += 2)
     {
         if (bitset_getbit(pole, i))  // skip prime numbers
             continue;
 
-        for (bitset_index_t n = i+i; n < available_values; n += i)
+        for (bitset_index_t n = i*i; n <= available_values; n += i*2)
             bitset_setbit(pole, n, 1);
     }
 }
