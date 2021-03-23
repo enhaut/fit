2eb2eee40f6670595b1f46b8f0168706d2f738d6
Samuel Dobroň <samulkodob@gmail.com>
2021-03-23 14:23:04 +0100

    working version

--- 1/src/steg-decode.c
+++ 1/src/steg-decode.c
@@ -4,16 +4,41 @@
 // Přeloženo: gcc 10.2.1
 //
 
-#include "steg-decode.h"
 #include "ppm.h"
 #include "bitset.h"
 #include "eratosthenes.h"
 #include <stdio.h>
 
 
+char *add_character_to_message(char *message, char character, unsigned index)
+{
+    unsigned message_size = index ? index : 100; // first message size has to be != 0
+    if (!message)
+    {
+        message = malloc(message_size + 1);
+    }
+    else if (index == message_size && character != 0)
+    {
+        message_size *= 2;
+        message = realloc(message, message_size + 1);
+    }
+    if (!message)
+    {
+        warning_msg("Nepodařilo se alokovat místo pro zprávu!");
+        return NULL;
+    }
+
+    message[index] = character;
+
+    return message;
+}
+
 void read_encoded_message(struct ppm *loaded_image, bitset_t primes)
 {
     char message_character = 0;
+    unsigned character_index = 0;
+    char *message = NULL;
+    int message_correct_ending = 0;
 
     for (size_t i = 23, char_index = 0; i < bitset_size(primes); i++)
     {
@@ -24,24 +49,32 @@ void read_encoded_message(struct ppm *loaded_image, bitset_t primes)
             message_character |= 1UL << char_index;
 
         char_index++;
-        if (char_index == CHAR_BIT && !message_character)
+
+        if (char_index == CHAR_BIT)
         {
-            printf("\n");
-            break;
-        }
-        else if (char_index == CHAR_BIT)
-        {
-            printf("%c", message_character);
+            message = add_character_to_message(message, message_character, character_index);
+
+            if (message_character == '\0')
+            {
+                message_correct_ending = 1;
+                break;
+            }
+
+            character_index++;
             char_index = 0;
             message_character = 0;
         }
-        else if (i + 1 == bitset_size(primes))
-        {
-            bitset_free(primes);
-            ppm_free(loaded_image);
-            error_exit("Zpráva není korektně zakončená!");
-        }
     }
+    if (!message_correct_ending)
+    {
+        bitset_free(primes);
+        free(message);
+        ppm_free(loaded_image);
+        error_exit("Zpráva není korektně zakončená!");
+    }
+
+    printf("%s\n", message);
+    free(message);
 }
 
 int main(int argc, char *args[])
