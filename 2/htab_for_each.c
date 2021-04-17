d0b8c9d3a7e8718d8fc81da0230400bb5a5d5b5e
Samuel Dobroň <samulkodob@gmail.com>
2021-04-17 23:05:46 +0200

    added htab_for_each()

---
+++ 2/htab_for_each.c
@@ -0,0 +1,25 @@
+#include "htab.h"
+#include "htab_private.h"
+
+/** @brief Function creates new hash table and moves values from old one.
+ * After that, origin hash table stays empty but not reallocated.
+ * @param t hash table.
+ * @param f function that is called for every item in table.
+*/
+void htab_for_each(const htab_t * t, void (*f)(htab_pair_t *data))
+{
+    if (!t || !f)
+        return;
+
+    for (size_t i = 0; i < t->arr_size; i++)
+    {
+        f(&(t->data[i]->element));  // user in worst scenario can free(key), but that's not my business
+
+        htab_item *same_index_item = t->data[i]->next;
+        while (same_index_item)
+        {
+            f(&(same_index_item->element));
+            same_index_item = same_index_item->next;
+        }
+    }
+}
