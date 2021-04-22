5a5da218e618e73df223f0a4f51318a752320003
Samuel Dobroň <samulkodob@gmail.com>
2021-04-22 17:13:08 +0200

    removed unused macro

--- 2/htab_private.h
+++ 2/htab_private.h
@@ -7,11 +7,8 @@
 #include <stdlib.h>
 #include "htab.h"
 
-// následující řádky zabrání násobnému vložení:
 #ifndef __HTAB_PRIVATE_H__
 #define __HTAB_PRIVATE_H__
-
-#define PRINT_ERROR(message) do{fprintf(stderr, message);}while(0)
 
 typedef struct htab_element {
     htab_pair_t element;
