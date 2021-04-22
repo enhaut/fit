92f6973c06536f30cfc65b9c3248561310cb02f7
Samuel Dobroň <samulkodob@gmail.com>
2021-04-22 17:06:48 +0200

    added HASHTEST function

--- 2/wordcount.c
+++ 2/wordcount.c
@@ -7,7 +7,27 @@
 #include "htab.h"
 #include "io.h"
 #define WORD_LIMIT 128
-#define HASH_TABLE_SIZE 128
+/*
+ * Hash table size should be at least 1.3x bigger than number of maximum
+ * values also it should be prime number.
+ * I don't know the number of words in provided files, so i just choose 1117.
+ * The more memory it uses the faster it is, so we have to find a compromise.
+*/
+#define HASH_TABLE_SIZE 1117
+
+#ifdef HASHTEST
+// djb2 hash algo from http://www.cse.yorku.ca/~oz/hash.html
+size_t htab_hash_function(const char *str)
+{
+    unsigned long hash = 5381;
+    int c;
+
+    while ((c = *str++))
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+    return hash;
+}
+#endif
 
 void print_result(htab_pair_t *pair)
 {
