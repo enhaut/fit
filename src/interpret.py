4d0b0b1b424ebe25278df10abc97ea9af2d5b8dc
Samuel Dobron <xdobro23@stud.fit.vut.cz>
2022-03-13 23:55:54 +0100

    parsing instruction arguments

--- src/interpret.py
+++ src/interpret.py
@@ -3,6 +3,7 @@ import sys
 from dataclasses import dataclass
 from typing import Union, List
 import xml.etree.ElementTree as ET
+import re
 
 
 def error_exit(message: str, code: int):
@@ -23,17 +24,76 @@ class MemoryFrame:
         self._parent = parent
 
 
+class ArgumentType:
+    def __init__(self, name: str, element: ET.Element):
+        self.name = name
+        self.value = None
+        self.raw_element = element
+
+    def __repr__(self):
+        return f"{self.__class__.__name__} - {self.name}: {self.value}"
+
+    def set_value(self):
+        raise NotImplementedError
+
+
+class VariableArgument(ArgumentType):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.frame = self.get_frame()
+        self.type = self.get_type()
+
+    def set_value(self):
+        pass
+
+    def get_frame(self):
+        pass
+
+    def get_type(self):
+        pass
+
+
+class ConstantArgument(ArgumentType):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.type = self.get_type()
+
+    def set_value(self):
+        pass
+
+    def get_type(self):
+        pass
+
+
+class LabelArgument(ArgumentType):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def set_value(self):
+        pass
+
+
+class TypeArgument(ArgumentType):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def set_value(self):
+        pass
+
+
 class Instruction:
     __VAR_NAME_REGEXP = "[a-zA-Z_\-$&%*!?][a-zA-Z0-9_\-$&%*!?]*"
-    __CONSTANTS_REGEXP= "(?:int@(?:0[xX][0-9a-fA-F]+|[+-]?[0-9]+))|bool@(?:true|false)|nil@nil|string@(?:[^#\\\\\s]|\\\\\d{3})*"
+    __CONSTANTS_REGEXP= "(?:(?:0[xX][0-9a-fA-F]+|[+-]?[0-9]+))|(?:true|false)|nil|(?:[^#\\\\\s]|\\\\\d{3})*"
     VAR_REGEXPS = {
-        "var": "\s+([LTG]F@" + __VAR_NAME_REGEXP + ")",
-        "label": "\s+("  + __VAR_NAME_REGEXP + ")",
-        "symb": "\s+((?:[LTG]F@"  + __VAR_NAME_REGEXP + ")|(?:"  + __CONSTANTS_REGEXP + "))",
-        "type": "\s+(int|string|bool)"
+        "var": "([LTG]F@" + __VAR_NAME_REGEXP + ")",
+        "label": "(" + __VAR_NAME_REGEXP + ")",
+        "symb": "((?:[LTG]F@" + __VAR_NAME_REGEXP + ")|(?:" + __CONSTANTS_REGEXP + "))",
+        "type": "(int|string|bool)"
     }
     INSTRUCTIONS = [
-        [("(CREATEFRAME)",), ("(PUSHFRAME)",), ("(POPFRAME)",), ("(RETURN)",), ("(BREAK)",)],
+        [("CREATEFRAME",), ("PUSHFRAME",), ("POPFRAME",), ("RETURN",), ("BREAK",)],
         [
             ("DEFVAR", VAR_REGEXPS["var"]),
             ("CALL", VAR_REGEXPS["label"]),
@@ -73,27 +133,129 @@ class Instruction:
     ]
 
     def __init__(self, name: str, xml_raw: ET.ElementTree):
-        pass
+        self.name = name.upper()
+        self.raw_instruction = xml_raw
+        self.ins_regexp = self.get_instruction_regexp()
+
+        self.arg1: ArgumentType = None
+        self.arg2: ArgumentType = None
+        self.arg3: ArgumentType = None
+        self.set_arguments()
+
+    def __repr__(self):
+        ret = f"{self.__class__.__name__} - {self.name} \n"
+        ret += f"\t{self.arg1}\n"
+        ret += f"\t{self.arg2}\n"
+        ret += f"\t{self.arg3}"
+        return ret
+
+    @staticmethod
+    def check_parameter_attributes(parameter: ET.Element):
+        if "type" in parameter.keys() and len(parameter.items()) == 1:
+            return True
+        error_exit("Invalid param attributes", 31)
+
+    def __get_instruction_array_index(self):
+        if isinstance(self, NoArgsInstruction):
+            return 0
+        elif isinstance(self, SingleArgsInstruction):
+            return 1
+        elif isinstance(self, DoubleArgsInstruction):
+            return 2
+        elif isinstance(self, TripleArgsInstruction):
+            return 3
+        else:
+            raise NotImplementedError("Instruction with >3 arguments is not (yet) supported")
+
+    def get_instruction_regexp(self):
+        instructions_array = self.INSTRUCTIONS[self.__get_instruction_array_index()]
+
+        for instruction_array in instructions_array:
+            if instruction_array[0] != self.name:
+                continue
+            return instruction_array
+
+        error_exit(f"Invalid instruction: {self.name}", 32)
+
+    def set_arguments(self):
+        raise NotImplementedError
+
+    def check_argument(self, attr_id: int, attribute_element: ET.Element):
+        attr_id += 1  # first member of array is instruction name
+
+        if not re.match(self.ins_regexp[attr_id], attribute_element.text, re.UNICODE):
+            error_exit("Invalid attribute type!", 53)
+
+    def get_argument(self, element: ET.Element) -> ArgumentType:
+        if element.attrib["type"] == "label":
+            arg_class = LabelArgument
+        elif element.attrib["type"] == "var":
+            arg_class = VariableArgument
+        elif element.attrib["type"] == "int" or element.attrib["type"] == "bool" or \
+            element.attrib["type"] == "string" or element.attrib["type"] == "nil":
+            arg_class = ConstantArgument
+        elif element.attrib["type"] == "type":
+            arg_class = TypeArgument
+        else:
+            raise NotImplementedError("Other attribute types are not (yet) implemented!")
+
+        return arg_class(element.text, element)
 
 
 class NoArgsInstruction(Instruction):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, *kwargs)
 
+    def set_arguments(self):
+        pass
+
 
 class SingleArgsInstruction(Instruction):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, *kwargs)
+
+    def set_arguments(self):
+        arg = self.raw_instruction.find("arg1")
+        self.check_parameter_attributes(arg)
+
+        self.check_argument(0, arg)
+        self.arg1 = self.get_argument(arg)
 
 
 class DoubleArgsInstruction(Instruction):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, *kwargs)
 
+    def set_arguments(self):
+        arg = self.raw_instruction.find("arg1")
+        arg2 = self.raw_instruction.find("arg2")
+        self.check_parameter_attributes(arg)
+        self.check_parameter_attributes(arg2)
+
+        self.check_argument(0, arg)
+        self.check_argument(1, arg2)
+        self.arg1 = self.get_argument(arg)
+        self.arg2 = self.get_argument(arg2)
+
 
 class TripleArgsInstruction(Instruction):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, *kwargs)
+
+    def set_arguments(self):
+        arg = self.raw_instruction.find("arg1")
+        arg2 = self.raw_instruction.find("arg2")
+        arg3 = self.raw_instruction.find("arg3")
+        self.check_parameter_attributes(arg)
+        self.check_parameter_attributes(arg2)
+        self.check_parameter_attributes(arg3)
+
+        self.check_argument(0, arg)
+        self.check_argument(1, arg2)
+        self.check_argument(2, arg3)
+        self.arg1 = self.get_argument(arg)
+        self.arg2 = self.get_argument(arg2)
+        self.arg3 = self.get_argument(arg3)
 
 
 class Interpret:
@@ -162,15 +324,16 @@ class Interpret:
                 error_exit("Invalid instruction argument", 31)
 
         params_count = len(children)
+        name = instruction.attrib["opcode"]  # TODO: check for opcode attr
 
         if params_count == 0:
-            return instruction.tag, NoArgsInstruction
+            return name, NoArgsInstruction
         elif params_count == 1:
-            return instruction.tag, SingleArgsInstruction
+            return name, SingleArgsInstruction
         elif params_count == 2:
-            return instruction.tag, DoubleArgsInstruction
+            return name, DoubleArgsInstruction
         elif params_count == 3:
-            return instruction.tag, TripleArgsInstruction
+            return name, TripleArgsInstruction
         else:
             error_exit("Invalid arguments count", 31)
 
