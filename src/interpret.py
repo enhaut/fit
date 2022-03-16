70709aa58eebde4126196718162785e7d3383084
Samuel Dobron <xdobro23@stud.fit.vut.cz>
2022-03-16 22:55:27 +0100

    implemented `STRI2INT`

--- src/interpret.py
+++ src/interpret.py
@@ -636,6 +636,27 @@ class InstructionOR(LogicalInstruction):
         return first or second
 
 
+class InstructionSTRI2INT(TripleArgsInstruction):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def interpret(self, memory: Dict[str, List[MemoryFrame]]):
+        result = self._get_variable(self.arg1.name, memory)
+        if result.initialized and result.var_type != str:
+            error_exit(f"Invalid target variable type: {result.name}", 53)
+
+        first = self._get_value_from_symb(self.arg2, memory)
+        second = self._get_value_from_symb(self.arg3, memory)
+
+        if not isinstance(first, str) or not isinstance(second, int):
+            error_exit(f"Invalid operand {self.arg2.name} or {self.arg3.name} types!", 53)
+
+        if second >= len(first):
+            error_exit(f"Index {self.arg3.name} out of range in {self.arg2.name}", 58)
+
+        self.set_value(result, ord(first[second]))
+
+
 class Interpret:
     def __init__(self):
         self.arg_parser = argparse.ArgumentParser(description='IPPcode22 interpret')
