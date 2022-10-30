035e8aa7fcf2b5fa12fe1592275b970975ca3600
Samuel Dobron <xdobro23@stud.fit.vutbr.cz>
2022-10-30 16:38:34 +0100

    project 1: graph implementation

--- project/izv-part01/part01.py
+++ project/izv-part01/part01.py
@@ -29,8 +29,44 @@ def integrate(x: np.array, y: np.array) -> float:
     )
 
 
-def generate_graph(a: List[float], show_figure: bool = False, save_path: str | None=None):
-    pass
+def generate_graph(
+    a: List[float], show_figure: bool = False, save_path: str | None = None
+):
+    colors = ["blue", "orange", "green"]
+    x = np.linspace(-3, 3, 100)
+    xsq = x**2
+    ys = xsq * np.array([a]).T
+
+    fig = plt.figure(figsize=(6, 4))
+    ax = fig.add_subplot()
+    ax.margins(0, 0)
+    ax.spines["top"].set_bounds(-3, 4.2)
+    ax.spines["bottom"].set_bounds(-3, 4.2)
+    ax.spines["right"].set_position(("data", 4.2))
+
+    ax.set_ylim(-20, 20)
+    ax.set_ylabel(r"$f_{a}(x)$")
+
+    for i in range(a_len := len(a)):
+        ax.plot(
+            x, ys[i], color=colors[i % 3], label=rf"$\gamma_{ {a[i]} }(x)$"
+        )
+        ax.fill_between(
+            x, ys[i], color=colors[i % 3], alpha=0.1, label="_nolegend_"
+        )
+        ax.annotate(
+            rf"$\int f_{ {a[i]} }(x)dx$",
+            xy=(x[-1], ys[i, -1]),
+            xytext=(0, -4),
+            textcoords="offset points",
+        )
+
+    fig.legend(loc="upper center", ncol=a_len)
+
+    if show_figure:
+        plt.show()
+    elif save_path:
+        fig.savefig(save_path, bbox_inches="tight")
 
 
 def generate_sinus(show_figure: bool=False, save_path: str | None=None):
