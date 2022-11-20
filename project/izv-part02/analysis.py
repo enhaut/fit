0d86037848d968cc65b711887e03facbd33f1a0a
Samuel Dobron <xdobro23@stud.fit.vutbr.cz>
2022-11-20 22:00:07 +0100

    SQ

--- project/izv-part02/analysis.py
+++ project/izv-part02/analysis.py
@@ -98,18 +98,15 @@ def parse_data(df: pd.DataFrame, verbose: bool = False) -> pd.DataFrame:
     df["date"] = pd.to_datetime(df["date"], errors="coerce")
 
     to_int_cols = [
-        "l", "n", "weekday(p2a)", "r", "s", "p19", "p5a", "p6", "p8", "p9", "p10", "p11", "p12",
-        "p15", "p16", "p17", "p18", "p20", "p21", "p22", "p23", "p24", "p27", "p28", "p7",
+        "l", "n", "weekday(p2a)", "r", "s", "p19", "p5a", "p6", "p8", "p9", "p10", "p11", "p12", "p13c",
+        "p15", "p16", "p17", "p18", "p20", "p21", "p22", "p23", "p24", "p27", "p28", "p7", "p13a", "p13b",
         "p34", "p35", "p36", "p37", "p39", "p44", "p45a", "p47", "p48a", "p49", "p50a", "p51",
         "p52", "p55a", "p57", "p58"
     ]
     for col in to_int_cols:
         df[col] = pd.to_numeric(df[col], errors="coerce", downcast="integer")
 
-    categorized_cols = ["p36", "weekday(p2a)", "p2b", "p6", "p7", "p8", "p8", "p10", "p11", "p12", "p13a", "p13b",
-                        "p13c", "p15", "p16", "p17", "p18", "p19", "p20", "p21", "p22", "p23", "p24", "p27", "p28",
-                        "p34", "p35", "p39", "p44", "p45a", "p47", "p48a", "p49", "p50a", "p50b", "p50b", "p51", "p52",
-                        "p55a", "p57", "p58", "k", "p", "t", "p5a", "q", "i", "h"] + to_int_cols
+    categorized_cols = ["q", "i", "h", "k", "p", "t"]
     for col in categorized_cols:
         df[col] = df[col].astype("category")
 
