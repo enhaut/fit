be0ceb7b0003e7a988aaf52b6361a5a0a2ce8ff5
Samuel Dobron <xdobro23@stud.fit.vut.cz>
2023-10-26 17:39:19 +0200

    vectorization/Line: prefetching, early calculation stop

--- projects/vectorization/calculators/LineMandelCalculator.cc
+++ projects/vectorization/calculators/LineMandelCalculator.cc
@@ -41,41 +41,62 @@ LineMandelCalculator::~LineMandelCalculator() {
 	data = NULL;
 }
 
-int * LineMandelCalculator::calculateMandelbrot () {
-	float real, r2, i2, y;
 
-	for (int i = 0; i < height; i++)
+int * LineMandelCalculator::calculateMandelbrot () {
+	float real, r2, i2, y, i_f, j_f;
+
+	i_f = 0.0f;
+	j_f = 0.0f;
+	
+	for (int i = 0; i < height/2; i++)
 	{
-		y = y_start + i * dy; // current imaginary value
+		y = y_start + i_f * dy; // current imaginary value
 
 		memset(processed, 0, width * sizeof(int));
-		memset(zImagf, y, width * sizeof(float));
+
+		#pragma omp simd
 		for (int x = 0; x < width; x++)
 		{
 			zImagf[x] = y;
 			zRealf[x] = x_start + x * dx;
 		}
+
 	  	for (int k = 0; k < limit; ++k)
 		{
 			#pragma omp simd
 		  	for (int j = 0; j < width; j++)
 	  	  	{
-				real = x_start + j * dx;
+				__builtin_prefetch(zRealf + 1);
+				__builtin_prefetch(zImagf + 1);
+				__builtin_prefetch(processed + 1);
+
+				real = x_start + j_f * dx;
 
 		  	  	r2 = zRealf[j] * zRealf[j];
 		  	  	i2 = zImagf[j] * zImagf[j];
 
-		  	  	if (r2 + i2 > 4.0f && !processed[j])
+		  	  	if (!processed[j] && r2 + i2 > 4.0f)
 		  	  	{
 					processed[j] = 1;
 		  	  	  	*(data + i*width + j) = k;
+		  	  	  	*(data + (height-1)*width-(i*width) + j) = k;
 				}
 
 		  	  	zImagf[j] = 2.0f * zRealf[j] * zImagf[j] + y;
 		  	  	zRealf[j] = r2 - i2 + real;
+		  	  	j_f++;
 	  	  	}
-	
+	  	  	j_f = 0.0f;
+
+		    int sum = 0;
+            #pragma omp simd reduction(+:sum)
+            for (int x = 0; x < width; x++)
+              sum = sum + processed[x];
+
+            if (sum >= width)
+              break;
 		}
+		i_f++;
 	}
 	
 	return data;
