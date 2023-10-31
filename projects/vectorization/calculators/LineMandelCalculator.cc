3e076b65c3f62fa10d1987cf71917033ff2ec054
Samuel Dobron <xdobro23@stud.fit.vut.cz>
2023-10-31 18:36:53 +0100

    vectorization/Line: retype of loop iter values

--- projects/vectorization/calculators/LineMandelCalculator.cc
+++ projects/vectorization/calculators/LineMandelCalculator.cc
@@ -51,9 +51,10 @@ LineMandelCalculator::~LineMandelCalculator() {
 }
 
 int * LineMandelCalculator::calculateMandelbrot () {
-	float real, r2, i2, y, i_f, j_f;
+	float real, r2, i2, y, i_f = 0.0f, j_f = 0.0f;
 
 	float x_start_f = x_start;
+	float y_start_f = y_start;
 	float dx_f = dx;
 	float dy_f = dy;
 	// ^^ retype doubles to floats to remove implicit recasting in loops
@@ -62,33 +63,31 @@ int * LineMandelCalculator::calculateMandelbrot () {
 	float *Imf = zImagf;
 	int *proc = processed;
 
-	i_f = 0.0f;
-	j_f = 0.0f;
-	
-	for (int i = 0; i < height/2; i++)
+	unsigned int i, j, k, processed_count;  // 20ms performance gain 
+	for (i = 0; i < height/2; i++)
 	{
-		y = y_start + i_f * dy; // current imaginary value
+		y = y_start_f + i_f * dy_f; // current imaginary value
 
 		memset(processed, 0, width * sizeof(int));
 
-		#pragma omp simd aligned(Rlf, Imf, proc: CACHE_LINE_SIZE)
-		for (int x = 0; x < width; x++)
+		#pragma omp simd reduction(+: j) aligned(Rlf, Imf, proc: CACHE_LINE_SIZE)
+		for (j = 0; j < width; j++)
 		{
-			Imf[x] = y;
-			Rlf[x] = x_start_f + x * dx_f;
+			Imf[j] = y;
+			Rlf[j] = x_start_f + j * dx_f;
 		}
 
-		uint processed_count = 0;
-	  	for (int k = 0; k < limit; ++k)
+		processed_count = 0;
+	  	for (k = 0; k < limit; ++k)
 		{
-			#pragma omp simd reduction(+: processed_count, j_f) aligned(Rlf, Imf, proc: CACHE_LINE_SIZE)
-		  	for (int j = 0; j < width; j++)
+			#pragma omp simd reduction(+: processed_count, j_f, j) aligned(Rlf, Imf, proc: CACHE_LINE_SIZE)
+		  	for (j = 0; j < width; j++)
 	  	  	{
 				__builtin_prefetch(Rlf + 1);
 				__builtin_prefetch(Imf + 1);
 				__builtin_prefetch(proc + 1);
 
-				real = x_start_f + j_f * dx_f;
+				real = x_start_f + j_f * dx_f;  // having it here is faster than moving it bellow if
 
 		  	  	r2 = Rlf[j] * Rlf[j];
 		  	  	i2 = Imf[j] * Imf[j];
@@ -103,6 +102,7 @@ int * LineMandelCalculator::calculateMandelbrot () {
 
 		  	  	Imf[j] = 2.0f * Rlf[j] * Imf[j] + y;
 		  	  	Rlf[j] = r2 - i2 + real;
+
 		  	  	j_f++;
 	  	  	}
 	  	  	j_f = 0.0f;
