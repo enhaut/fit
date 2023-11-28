8febd61341f982acca1a609cd0d625415fce80ac
Samuel Dobroň <xdobro23@stud.fit.vutbr.cz>
2023-11-28 17:23:07 +0100

    parallelism: parallelization of marchCubes algo

--- projects/parallelism/parallel_builder/loop_mesh_builder.cpp
+++ projects/parallelism/parallel_builder/loop_mesh_builder.cpp
@@ -28,6 +28,8 @@ unsigned LoopMeshBuilder::marchCubes(const ParametricScalarField &field)
     unsigned totalTriangles = 0;
 
     // 2. Loop over each coordinate in the 3D grid.
+    #pragma omp parallel for default(none) shared(totalCubesCount, field) \
+    reduction(+:totalTriangles)  schedule(dynamic, 32)
     for(size_t i = 0; i < totalCubesCount; ++i)
     {
         // 3. Compute 3D position in the grid.
